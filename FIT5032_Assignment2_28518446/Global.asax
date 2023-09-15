c7c3359bd8a0b5be36f47c5206dcbcd356a6f7ec
jiahao wan <jwan0177@student.monash.edu>
2023-09-15 20:38:27 +1000

    Add project files.

---
+++ FIT5032_Assignment2_28518446/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_Assignment2_28518446.MvcApplication" Language="C#" %>
---
+++ FIT5032_Assignment2_28518446/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_Assignment2_28518446.Startup))]
+namespace FIT5032_Assignment2_28518446
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
